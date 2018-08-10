be3df1e11d72ac3a0db160f5ee5aaaca64ea2314
Ryan Rapini <ryanrapini@gmail.com>
2018-08-10 13:47:55 -0400

    adding vendors

--- app/League.php
+++ app/League.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Team;
 
 class League extends Model
 {
-    //
+    public function teams() {
+    	return $this->hasMany('App\Team');
+    }
 }
